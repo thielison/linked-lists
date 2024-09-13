44bc0f4457c5f341420102d78677459d7302a609
Thielison <67246254+thielison@users.noreply.github.com>
2024-09-13 20:43:25 -0300

    Add linked list methods: append and toString

--- linked-list.js
+++ linked-list.js
@@ -10,6 +10,41 @@ class LinkedList {
         this.head = new Node();
         this.listSize = 0;
     }
+
+    // Adds a new node containing value to the end of the list
+    append(value) {
+        const node = new Node(value);
+
+        if (this.listSize === 0) {
+            this.head = node;
+        } else {
+            let current = this.head;
+
+            while (current.nextNode) {
+                current = current.nextNode;
+            }
+
+            current.nextNode = node;
+        }
+
+        this.listSize += 1;
+    }
+
+    toString() {
+        let string = "";
+        let current = this.head;
+
+        while (current) {
+            string += `( ${current.value} ) => `;
+
+            current = current.nextNode;
+        }
+
+        // End of list
+        string += `( ${current} )`;
+
+        console.log(string);
+    }
 }
 
 export default LinkedList;
--- main.js
+++ main.js
@@ -2,9 +2,11 @@ import LinkedList from "./linked-list.js";
 
 const list = new LinkedList();
 
-// list.append("dog");
-// list.append("cat");
-// list.append("parrot");
-// list.append("hamster");
-// list.append("snake");
-// list.append("turtle");
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+list.toString();
